1ed2bb8c2961d9658ebcc8368fb848f05c6d24d3
MusDev13 <sonics0977703996@gmail.com>
2026-02-08 18:05:39 +0200

    fixed delete status

--- src/core/worlds/delete/delete.rs
+++ src/core/worlds/delete/delete.rs
@@ -3,6 +3,7 @@ use std::fs::{remove_dir_all};
 use crate::core::{worlds::list::{get_path::get_path, is_exist::is_exist}};
 
 pub fn delete(save_name: &str)->bool{
-  if is_exist(save_name){let _ = remove_dir_all(get_path(save_name)).is_ok();}
+    //println!("{}", get_path(save_name).display());
+  if is_exist(save_name){let _ = remove_dir_all(get_path(save_name)).is_ok(); return true;}
   false
 }
